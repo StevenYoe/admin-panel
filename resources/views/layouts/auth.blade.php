fa2de5d349293f906e38b678561ea9077c2fc18d
Steven <stevenyoe80@gmail.com>
2025-04-28 16:57:52 +0700

    title name

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Pazar User Admin')</title>
+    <link rel="icon" href="img/Logo.ico" type="image/x-icon">
     <!-- Tailwind CSS via CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- SweetAlert2 -->
